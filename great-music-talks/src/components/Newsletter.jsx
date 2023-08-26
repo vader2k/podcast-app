6f32482158d60cd776e9ffcd424fadd3f29c2391
vader2k <76834964+vader2k@users.noreply.github.com>
2023-08-26 18:40:12 +0100

    test and see

--- great-music-talks/src/components/Newsletter.jsx
+++ great-music-talks/src/components/Newsletter.jsx
@@ -4,11 +4,11 @@ import { mail } from '../assets'
 
 const Newsletter = () => {
   return (
-    <div className='bg-orange-600 flex flex-col sm:gap-5'>
+    <div className='bg-orange-600 flex flex-col sm:gap-5 letter'>
       <div className='flex sm:flex-row flex-col  items-center text-white gap-5'>
         <img src={mail}/>
         <div>
-          <p className='text-black'>Get early access to the new episodes.</p>
+          <p className='text-white'>Get early access to the new episodes.</p>
           <p className='sm:text-[2rem] text-[1.5rem] py-3'>Subscribe to the newsletter!</p>
         </div>
       </div>
